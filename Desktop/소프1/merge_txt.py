338df7ee82b5d18058cb10ac5e20d607ed5b4db2
yihwa moon <ansdlghk1026@gmail.com>
2026-04-23 11:46:02 +0900

    파일 위치 최상위로 고정

--- Desktop/소프1/merge_txt.py
+++
@@ -1,9 +0,0 @@
-import os
-txt_files = [f for f in os.listdir('.') if f.endswith('.txt')]
-txt_files.sort()
-with open('merged.txt', 'w', encoding='utf-8') as outfile:
-    for f in txt_files:
-        outfile.write(f"\n\n{'='*20}\nFILE: {f}\n{'='*20}\n\n")
-        with open(f, 'r', encoding='utf-8') as infile:
-            outfile.write(infile.read())
-print("Merged into merged.txt")
